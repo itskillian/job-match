0e4923a7a6fa4269d065b4a96f7d510ac886b9a7
killian <killiancodes@gmail.com>
2025-02-14 11:44:00 -0800

    install shadcn/ui

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,22 @@
-import "./globals.css";
+import '@/app/globals.css';
+import { ThemeProvider } from '@/components/theme-provider';
 
 export const metadata = {
-  title: "ResumeMatch",
+  title: 'ResumeMatch',
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className="">
-        {children}
+    <html lang='en' suppressHydrationWarning>
+      <body>
+        <ThemeProvider
+          attribute='class'
+          defaultTheme='system'
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
--- app/page.js
+++ app/page.js
@@ -1,9 +1,14 @@
 import Resume from '@/components/resume';
-
+import ThemeToggle from '@/components/theme-toggle';
 export default function Home() {
   return (
-    <div className="flex flex-col items-center min-w-[320px] max-w-[480px] p-4 mx-auto h-screen">
-      <Resume />
+    <div className='flex flex-col items-center min-w-[320px] max-w-[480px] p-4 mx-auto h-screen'>
+      <div className='flex justify-end w-full'>
+        <ThemeToggle /> 
+      </div>
+      <div className='flex flex-col items-center justify-center w-full h-3/4'>
+        <Resume />
+      </div>
     </div>
   )
 }
